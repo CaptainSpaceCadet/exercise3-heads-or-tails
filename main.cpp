776de345b31b020d792416a67ec608e439ccc871
Joan Quinn <joan-gitcommit@anarchistturtle.com>
2024-07-04 21:16:40 +0900

    HoT: initial revision

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <cstdlib>
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+bool isFlippedCoinHeads() {
+	return rand() % 2;
+}
+
+void runGame(int rounds) {
+	// initialised the pseudorandom sequence with the time
+	srand(time(NULL));
+
+	cout << "Tossing a coin..." << endl;
+	
+	int headCount = 0;
+	
+	for (int i = 1; i <= rounds; i += 1) {
+		cout << "Round " << i << ": ";
+		
+		if (isFlippedCoinHeads()) {
+			headCount += 1;
+			cout << "Heads" << endl;
+		} else {
+			cout << "Tails" << endl;
+		}
+	}
+	
+	cout << "Heads: " << headCount << ", Tails: " << rounds - headCount << endl;
+}
+
+int main() {
+	runGame(3);
+}
